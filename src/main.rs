1d8a8115aa4fd84b58e55685679c5a6bd9d5e19d
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2023-05-23 11:37:08 +0300

    Implement bcalm2 graph loading.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use clap::Parser;
+use genome_graph::compact_genome::implementation::DefaultSequenceStore;
+use genome_graph::compact_genome::interface::alphabet::dna_alphabet::DnaAlphabet;
+use genome_graph::io::bcalm2::read_bigraph_from_bcalm2_as_edge_centric_from_file;
+use genome_graph::types::PetBCalm2EdgeGraph;
 use log::{info, LevelFilter};
 use simplelog::{ColorChoice, CombinedLogger, Config, TermLogger, TerminalMode};
 use std::path::PathBuf;
@@ -8,6 +12,10 @@ struct Cli {
     /// The input file containing a node-centric de Bruijn graph.
     #[clap(long)]
     input: PathBuf,
+
+    /// The k-mer size used to generate the de Bruijn graph.
+    #[clap(short)]
+    k: usize,
 
     /// The output file where the arc-centric de Bruijn graph should be written to.
     #[clap(long)]
@@ -33,6 +41,11 @@ pub fn initialise_logging(log_level: LevelFilter) {
 fn main() {
     let cli = Cli::parse();
     initialise_logging(cli.log_level);
+
+    let mut sequence_store = DefaultSequenceStore::<DnaAlphabet>::new();
+    let _graph: PetBCalm2EdgeGraph<_> =
+        read_bigraph_from_bcalm2_as_edge_centric_from_file(&cli.input, &mut sequence_store, cli.k)
+            .unwrap();
 
     println!("Hello, world!");
 }
