468817b27d1b5f8fb678da76f92a00851b2e38ae
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2023-05-23 11:23:59 +0300

    Set up logging and CLI.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::path::PathBuf;
+use clap::Parser;
+use log::{info, LevelFilter};
+use simplelog::{ColorChoice, CombinedLogger, Config, TerminalMode, TermLogger};
+
+#[derive(Parser, Debug)]
+struct Cli {
+    /// The input file containing a node-centric de Bruijn graph.
+    #[clap(long)]
+    input: PathBuf,
+
+    /// The output file where the arc-centric de Bruijn graph should be written to.
+    #[clap(long)]
+    output: PathBuf,
+
+    /// The desired log level.
+    #[clap(long, default_value = "Info")]
+    log_level: LevelFilter,
+}
+
+
+pub fn initialise_logging(log_level: LevelFilter) {
+    CombinedLogger::init(vec![TermLogger::new(
+        log_level,
+        Config::default(),
+        TerminalMode::Mixed,
+        ColorChoice::Auto,
+    )])
+        .unwrap();
+
+    info!("Logging initialised successfully");
+}
+
 fn main() {
+    let cli = Cli::parse();
+    initialise_logging(cli.log_level);
+
     println!("Hello, world!");
 }
